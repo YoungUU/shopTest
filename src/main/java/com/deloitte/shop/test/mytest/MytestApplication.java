0fc831656de674f962217a9d3f36b3150d964f77
myshoptest.git <595125509@qq.com>
2018-08-03 09:25:48 +0800

    更新

--- src/main/java/com/deloitte/shop/test/mytest/MytestApplication.java
+++ src/main/java/com/deloitte/shop/test/mytest/MytestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MytestApplication {
-
+	//主程序运行
 	public static void main(String[] args) {
 		SpringApplication.run(MytestApplication.class, args);
 	}
